cb46e03ad5fdd96c2ae1576bf3424499abe72cf2
Subash G <gundapatinasubash@gmail.com>
2026-07-13 11:04:38 +0530

    Create statistics.py

---
+++ statistics.py
@@ -0,0 +1,45 @@
+from collections import Counter
+
+
+def generate_statistics(report_data):
+
+    severity_counter = Counter()
+    event_counter = Counter()
+    tactic_counter = Counter()
+
+    for report in report_data:
+
+        severity_counter[
+            report["severity"]
+        ] += 1
+
+        event_counter[
+            report["event"]
+        ] += 1
+
+        tactic_counter[
+            report["tactic"]
+        ] += 1
+
+    return {
+        "total_events":
+            len(report_data),
+
+        "critical":
+            severity_counter["CRITICAL"],
+
+        "high":
+            severity_counter["HIGH"],
+
+        "medium":
+            severity_counter["MEDIUM"],
+
+        "low":
+            severity_counter["LOW"],
+
+        "most_common_event":
+            event_counter.most_common(1)[0][0],
+
+        "most_common_tactic":
+            tactic_counter.most_common(1)[0][0]
+    }
