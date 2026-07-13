7a664279601924602253c430bf144c71375ae19a
Subash G <gundapatinasubash@gmail.com>
2026-07-13 06:54:14 +0000

    created detection rules

---
+++ cloudtrail_event_library/detection_rules.py
@@ -0,0 +1,22 @@
+DETECTION_RULES = {
+
+    "DeleteTrail": {
+        "alert": "CloudTrail Tampering Detected",
+        "severity_override": "CRITICAL"
+    },
+
+    "CreateUser": {
+        "alert": "New IAM User Created",
+        "severity_override": "HIGH"
+    },
+
+    "AttachUserPolicy": {
+        "alert": "Privilege Modification Detected",
+        "severity_override": "HIGH"
+    },
+
+    "CreateBucket": {
+        "alert": "New S3 Bucket Created",
+        "severity_override": None
+    }
+}
